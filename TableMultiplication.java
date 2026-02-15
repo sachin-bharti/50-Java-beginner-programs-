9eeb82ee78e05cf9a674769c647b148267a576c5
sachin-bharti <sachinbharti.offcl@gmail.com>
2026-02-16 00:24:29 +0530

    added program to write multiplie

--- TableMultiplication.java
+++ TableMultiplication.java
@@ -1,7 +1,24 @@
 import java.util.*;
 public class TableMultiplication {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in); 
+        while(true){
+        System.out.println("Enter Number :(enter 0 to exit)");
+        int number = sc.nextInt();
+        if(number ==0) {
+            System.out.println("Exiting program.");
+                break;  
+        }
+            for(int i=1; i <=10; i++){
+            int result = number * i;
+            System.out.println(number + " X " + i + " = "+ result);
+
+        }
+    
         
+     }
+     sc.close();
+    
     }
+
 }
